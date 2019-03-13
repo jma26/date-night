5a8de8824ac709689065eafc091ffc64ed96356c
Jesse Ma <j.masonoma17@gmail.com>
2019-03-13 02:14:53 -0700

    Add catch-all route to point to index.html at server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+// PORT
+const PORT = 8080;
+
+// Serve static files from client-side
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Handle cors
+app.use(cors());
+// Handle requested data with body-parser, turn on object nesting
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Catch all requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../build/index.html'));
+})
+
 app.listen(() => {
-  console.log('Listening on port:', process.env.PORT || 8080);
+  console.log('Listening on port:', process.env.PORT || PORT);
 });
