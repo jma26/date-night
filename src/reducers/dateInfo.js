8ce3d0838ea001eeaffb15808db8b610e0f30f27
Jesse Ma <j.masonoma17@gmail.com>
2019-03-16 00:24:18 -0700

    Fix switch case in dateInfo.js reducer - forgot to spread state

--- src/reducers/dateInfo.js
+++ src/reducers/dateInfo.js
@@ -4,18 +4,22 @@ const dateInfo = (state = {}, action) => {
   switch(action.type) {
     case LOCATION:
       return {
+        ...state,
         location: action.text,
       }
     case DRINK_CHOICE:
       return {
+        ...state,
         drink: action.text,
       }
     case CUISINE_CHOICE:
       return {
+        ...state,
         cuisine: action.text,
       }
     case DESSERT_CHOICE:
       return {
+        ...state,
         dessert: action.text,
       }
     default:
