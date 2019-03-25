d5b2a756492fdd767d04d980f9a00f2c8606e09c
Jesse Ma <j.masonoma17@gmail.com>
2019-03-25 02:15:15 -0700

    Create axios instance with config object, now responds with yelp api results

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const path = require('path');
 const app = express();
 const axios = require('axios');
 
+// Enables use of .env variables
+// Bug? Idk why I can't use CRA .env variablers with REACT_APP prefix so had to use dotenv
+require('dotenv').config();
+
 // PORT
 const PORT = process.env.PORT || 5000;
 
@@ -17,20 +21,31 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Axios Instances
-const businessInstance = axios.create({
-  baseUrl: 'https://api.yelp.com/v3/'
-})
-businessInstance.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_YELP_KEY}`;
-
 app.post('/business', (req, res) => {
-    let parameters = {
-      ...req.body
-    }
-    console.log(parameters);
-  // businessInstance.get(`businesses/search`, {
+  let parameters = {
+    ...req.body
+  }
 
-  // })
+  let foodConfig = {
+    url: '/businesses/search',
+    baseURL: 'https://api.yelp.com/v3/businesses/search',
+    method: 'get',
+    params: {
+      term: `${parameters.cuisine} restaurants`,
+      limit: 50,
+      location: parameters.location
+    }
+  }
+
+  // Axios Instances
+  const businessInstance = axios.create(foodConfig)
+  businessInstance.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_YELP_KEY}`;
+
+  businessInstance.get().then((response) => {
+    console.log(response.data);
+  }).catch((error) => {
+    console.log(error);
+  })
   res.send('Response successful from /business');
 })
 
