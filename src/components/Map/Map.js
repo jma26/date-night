efee73af5408797326b409dce2551f01f21c748b
Jesse Ma <j.masonoma17@gmail.com>
2019-04-01 23:27:37 -0700

    Created new component, Map

---
+++ src/components/Map/Map.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../../css/About.css';
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div className="Map">
+      <h1>Testing</h1>
+    </div>
+  }
+}
+
+export default Map;
