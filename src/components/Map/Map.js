bcacede411c070b8469a77e8745690dfaf6232d5
Jesse Ma <j.masonoma17@gmail.com>
2019-04-03 00:27:06 -0700

    Initialize mapquest map setup, now displays on map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -9,10 +9,27 @@ class Map extends Component {
     super(props);
   }
 
+  componentDidMount() {
+    window.L.mapquest.key = `${process.env.REACT_APP_MAPQUEST_KEY}`;
+    var map = window.L.mapquest.map('map', {
+      center: [37.7749, -122.4194],
+      layers: window.L.mapquest.tileLayer('map'),
+      zoom: 12
+    });
+    // Position zoom control
+    map.zoomControl.setPosition('topright');
+
+  }
+
   render() {
+    const mapStyles = {
+      height: '100vh',
+      width: '100vw'
+    }
+
     return (
       <div className="Map">
-        <h1>Testing</h1>
+        <div id="map" style={mapStyles}></div>
       </div>
     )
   }
