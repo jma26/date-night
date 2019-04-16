7aa418447473cb842e43dc193aec7379940b0b6a
Jesse Ma <j.masonoma17@gmail.com>
2019-04-15 19:45:31 -0700

    Redirect to '/' if redux state is null or undefined in componentDidMount() on Map.js: Prevent rendering of mapquest with undefined error

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -10,15 +10,30 @@ class Map extends Component {
   }
 
   componentDidMount() {
-    window.L.mapquest.key = `${process.env.REACT_APP_MAPQUEST_KEY}`;
-    var map = window.L.mapquest.map('map', {
-      center: [37.7749, -122.4194],
-      layers: window.L.mapquest.tileLayer('map'),
-      zoom: 12
-    });
-    // Position zoom control
-    map.zoomControl.setPosition('topright');
+    // Redirect to '/' if redux state is undefined or null
+    // Prevents refreshing on '/map' that will end up throwing an undefined error
+    if (!this.props.drink || !this.props.restaurant || !this.props.dessert) {
+      this.props.history.push('/');
+    } else {
+      window.L.mapquest.key = `${process.env.REACT_APP_MAPQUEST_KEY}`;
+      var map = window.L.mapquest.map('map', {
+        center: [37.7749, -122.4194],
+        layers: window.L.mapquest.tileLayer('map'),
+        zoom: 12
+      });
+      // Position zoom control
+      map.zoomControl.setPosition('topright');
 
+      // Directions from multiple locations
+      var directions = window.L.mapquest.directions();
+      directions.route({
+        locations: [
+          {latLng: {lat: this.props.drink.coordinates.latitude, lng: this.props.drink.coordinates.longitude}},
+          {latLng: {lat: this.props.restaurant.coordinates.latitude, lng: this.props.restaurant.coordinates.longitude}},
+          {latLng: {lat: this.props.dessert.coordinates.latitude, lng: this.props.dessert.coordinates.longitude}}
+        ]
+      });
+    }
   }
 
   render() {
@@ -36,7 +51,11 @@ class Map extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return state;
+  return {
+    drink: state.yelpData.drink,
+    restaurant: state.yelpData.restaurant,
+    dessert: state.yelpData.dessert,
+  }
 }
 
 export default withRouter(connect(mapStateToProps, null)(Map));
