5ca811ef3fcd4526b2eee89bc2901cf68f57f852
Jesse Ma <j.masonoma17@gmail.com>
2019-04-15 23:31:32 -0700

    Initial setup of new component, Loading

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Loading">
+        <h1>Loading now</h1>
+      </div>
+    )
+  }
+}
+
+export default Loading;
