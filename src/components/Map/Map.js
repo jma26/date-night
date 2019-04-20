8d8d18a6defb7291bf243c21c7510ffea82e1f4e
Jesse Ma <j.masonoma17@gmail.com>
2019-04-20 14:52:15 -0700

    Set directionsLayer options for mapQuest map in Map.js

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -15,17 +15,64 @@ class Map extends Component {
     if (!this.props.drink || !this.props.restaurant || !this.props.dessert) {
       this.props.history.push('/');
     } else if (!this.props.isFetching && this.props.drink && this.props.restaurant && this.props.dessert) {
+
       window.L.mapquest.key = `${process.env.REACT_APP_MAPQUEST_KEY}`;
+
       var map = window.L.mapquest.map('map', {
         center: [37.7749, -122.4194],
         layers: window.L.mapquest.tileLayer('map'),
         zoom: 12
       });
-      // Position zoom control
+
       map.zoomControl.setPosition('topright');
 
-      // Directions from multiple locations
       var directions = window.L.mapquest.directions();
+
+      directions.setLayerOptions({
+        startMarker: {
+          icon: 'circle',
+          draggable: false,
+          iconOptions: {
+            size: 'sm',
+            primaryColor: '#1FC715',
+            secondaryColor: '#1FC715',
+            symbol: 'A'
+          },
+          title: 'Drink'
+        },
+        endMarker: {
+          icon: 'circle',
+          draggable: false,
+          iconOptions: {
+            size: 'sm',
+            primaryColor: '#E9304F',
+            secondaryColor: '#E9304F',
+            symbol: 'C'
+          },
+          title: 'Dessert'
+        },
+        waypointMarker: {
+          icon: 'circle',
+          draggable: false,
+          iconOptions: {
+            size: 'sm',
+            primaryColor: '#E84393',
+            secondaryColor: '#E84393',
+            symbol: 'B'
+          },
+          title: 'Restaurant'
+        },
+        routeRibbon: {
+          color: "#2aa6ce",
+          opacity: 1.0,
+          showTraffic: false
+        },
+        alternateRouteRibbon: {
+          opacity: 1.0
+        },
+      });
+
+      // Route directions from multiple locations
       directions.route({
         locations: [
           {latLng: {lat: this.props.drink.coordinates.latitude, lng: this.props.drink.coordinates.longitude}},
