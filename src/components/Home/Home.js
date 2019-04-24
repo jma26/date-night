7a39a0989dd0ee0e8b6a205b4f3fed4748ef2a75
Jesse Ma <j.masonoma17@gmail.com>
2019-04-23 19:20:14 -0700

    Update styling for max-width of dialog, restart modals of Map.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
     return (
       <div className="Home">
         <Header isTop={this.props.isTop} />
-        <Container fluid className="app__container pl-0 pr-0">
+        <Container fluid className="pl-0 pr-0">
           <div className="home__container">
             <h1 className="home__headline">Plan your perfect night</h1>
             <p className="home__content">With DateNight, planning becomes quick and easy</p>
