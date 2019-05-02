6474892af3d067354a63308619aa838be5dc304b
Jesse Ma <j.masonoma17@gmail.com>
2019-05-01 20:33:37 -0700

    Add content for Error.js plus initial styling

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import '../../css/Error.css';
 
 import { connect } from 'react-redux';
 
 const Error = () => (
   <div className="Error">
-    <p>Error encountered</p>
+    <h1 className="Error__404">404</h1>
+    <p>Sorry! Something went wrong with our site. Please visit the site and try again</p>
+    <Link to="/">Try again</Link>
   </div>
 );
 
