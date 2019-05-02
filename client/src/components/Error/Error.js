ae79d889f21263b70aecc3d8def80cf1d90f174a
Jesse Ma <j.masonoma17@gmail.com>
2019-05-01 20:59:17 -0700

    Add try again link to root page, update styling for more responsive error.js component

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -7,14 +7,14 @@ import { connect } from 'react-redux';
 const Error = () => (
   <div className="Error">
     <h1 className="Error__404">404</h1>
-    <p>Sorry! Something went wrong with our site. Please visit the site and try again</p>
-    <Link to="/">Try again</Link>
+    <p className="Error__content">Sorry! Something went wrong with our site</p>
+    <p className="Error__content">Please visit the site and try again</p>
+    <Link className="Error__retry-link" to="/">Try again</Link>
   </div>
 );
 
 const mapStateToProps = (state) => {
   return state;
 }
-
 
 export default withRouter(connect(mapStateToProps, null)(Error));
